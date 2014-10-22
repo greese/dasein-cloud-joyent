e92f71434c82819afb3911d8b9040c8e192e3246
Binod <binod.kc@enstratius.com>
2014-10-22 15:44:02 -0500

    Fix to revert back providerProductId to product name.

--- src/main/java/org/dasein/cloud/joyent/compute/Machine.java
+++ src/main/java/org/dasein/cloud/joyent/compute/Machine.java
@@ -209,7 +209,7 @@ public class Machine extends AbstractVMSupport<SmartDataCenter> {
             }
         }
         post.put("metadata.dsnTrueImage", withLaunchOptions.getMachineImageId());
-        post.put("metadata.dsnTrueProduct", withLaunchOptions.getStandardProductId());
+        post.put("metadata.dsnTrueProductName", withLaunchOptions.getStandardProductId());
         post.put("metadata.dsnDescription", withLaunchOptions.getDescription());
         String json = method.doPostString(provider.getEndpoint(), "machines", new JSONObject(post).toString());
 
@@ -330,7 +330,8 @@ public class Machine extends AbstractVMSupport<SmartDataCenter> {
                 else {
                     prd.setDescription(prd.getName());
                 }
-                prd.setProviderProductId(ob.getString("id"));
+                //Set provider product id to product name.
+                prd.setProviderProductId(prd.getName());
                 if( options != null) {
                     if( options.matches(prd) )
                         products.add(prd);
@@ -572,7 +573,7 @@ public class Machine extends AbstractVMSupport<SmartDataCenter> {
                         else if( name.equals("dsnTrueImage") ) {
                             vm.setProviderMachineImageId(md.getString(name));
                         }
-                        else if( name.equals("dsnTrueProduct") ) {
+                        else if( name.equals("dsnTrueProductName") ) {
                             vm.setProductId(md.getString(name));
                         }
                         else {
@@ -607,6 +608,7 @@ public class Machine extends AbstractVMSupport<SmartDataCenter> {
                 vm.setDescription(vm.getName());
             }
             discover(vm);
+            boolean isVMSmartOs = (vm.getPlatform().equals(Platform.SMARTOS));
             if( vm.getProductId() == null ) {
                 VirtualMachineProduct d = null;
                 int disk, ram;
@@ -615,7 +617,14 @@ public class Machine extends AbstractVMSupport<SmartDataCenter> {
                 ram = ob.getInt("memory");
                 for( VirtualMachineProduct prd : listProducts(vm.getArchitecture()) ) {
                     d = prd;
+                    boolean isProductSmartOs = prd.getName().contains("smartos");
                     if( prd.getRootVolumeSize().convertTo(Storage.MEGABYTE).intValue() == disk && prd.getRamSize().intValue() == ram ) {
+                        if (isVMSmartOs && !isProductSmartOs){
+                            continue;
+                        }
+                        if (!isVMSmartOs && isProductSmartOs){
+                            continue;
+                        }
                         vm.setProductId(prd.getProviderProductId());
                         break;
                     }
